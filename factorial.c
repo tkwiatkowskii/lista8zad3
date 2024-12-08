03710a6d407e5b29dc7d533e98749769726d5115
tmmalt <lomek2008@gmail.com>
2024-12-08 16:37:03 +0100

    Dodano obsluge int overflow

--- factorial.c
+++ factorial.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <limits.h>
 int factorial (int n, int acc) {
-	if (n == 0) return acc;
+        if (acc > INT_MAX / n) return -1; //sprawdza int overflow
+	if (n == 0 || n == 1) return acc;
 	return factorial(n - 1, acc * n);
 }
 int main() {
